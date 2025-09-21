af10bda9f084404fc6604e81478a70b04d91f0a4
shafeekrahman04 <shafeekrahman04@gmail.com>
2025-09-21 14:36:15 +0530

    file added

---
+++ src/main/java/com/example/event/booking/model/AppUser.java
@@ -0,0 +1,55 @@
+package com.example.event.booking.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "admin_user")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "MOBILE_NUMBER")
+    private String mobileNumber;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "ROLE")
+    private String role;
+
+    @Column(name = "DELETED")
+    private String deleted;
+
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @Column(name = "CREATED_ON")
+    private LocalDateTime createdOn;
+
+    @Column(name = "UPDATED_BY")
+    private String updatedBy;
+    @Column(name = "UPDATED_ON")
+    private LocalDateTime updatedOn;
+}
